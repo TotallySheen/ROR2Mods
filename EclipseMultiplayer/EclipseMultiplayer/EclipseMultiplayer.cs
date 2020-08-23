990921a4b1e67ee0286780ae53ee929ef88dde08
TotallySheen <47955002+TotallySheen@users.noreply.github.com>
2020-08-22 21:34:21 -0400

    Added v1.0.0 of EclipseMultiplayer

---
+++ EclipseMultiplayer/EclipseMultiplayer/EclipseMultiplayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+
+namespace EclipseMultiplayer
+{
+    [BepInPlugin("com.sheen.EclipseMultiplayer", "Eclipse Multiplayer", "1.0.0")]
+    public class EclipseMultiplayer : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            // Changing this value makes the range of default difficulties include all
+            // 8 levels of Eclipse, instead of just the default 3
+            DifficultyCatalog.standardDifficultyCount = 11;
+        }
+    }
+}
---
+++ EclipseMultiplayer/README.md
@@ -0,0 +1,15 @@
+# Eclipse Multiplayer
+This simple mod adds the 8 levels of Eclipse as selectable difficulties in singleplayer and multiplayer runs.
+
+## Installation
+Copy the `EclipseMultiplayer.dll` to your BepInEx plugins directory.
+Or just use a mod manager, it's way easier
+
+## Feedback
+Any and all feedback is appreciated.
+
+I've set up a simple Discord server to take feedback for my mods, found at [https://discord.gg/nfhzJad](https://discord.gg/nfhzJad)
+
+## Changelog
+### 1.0.0
+ - Initial Release
